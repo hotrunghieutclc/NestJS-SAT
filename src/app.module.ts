a9b98c71d29efba6dc2d30d34b8f76fc9d47e622
Tomoriu Akarin <lequocanh10000@github.com>
2025-10-29 17:17:28 +0700

    Start project

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule, SequelizeModuleOptions } from '@nestjs/sequelize';
+import { sequelizeConfig } from './config/sequelize.config';
+import { StartTimingMiddleware } from './common/middlewares/start-timing.middleware';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { JwtModule, } from '@nestjs/jwt';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    SequelizeModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): SequelizeModuleOptions =>
+        sequelizeConfig(configService),
+    }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<any>('JWT_EXPIRES_IN'),
+        },
+      }),
+      global: true,
+    }),
+    UserModule,
+    AuthModule,
+  ],
+})
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(StartTimingMiddleware).forRoutes({ path: '*', method: RequestMethod.ALL});
+  }
+}
+
